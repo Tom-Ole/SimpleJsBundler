faf281981c380ca17170845c5f83b717c10e3eb1
Tom-Ole Korbach <Tomolekorbach@gmail.com>
2025-12-19 21:24:38 +0100

    really dump 'lexer'

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -11,6 +12,7 @@ import (
 type Bundler struct {
 	indexPath string
 	jsPaths   []string
+	jsContent string
 }
 
 func (bundler *Bundler) iterateFolder(dir string, indexName string) {
@@ -42,7 +44,54 @@ func (bundler *Bundler) iterateFolder(dir string, indexName string) {
 	}
 }
 
-func (bundler *Bundler) print() {
+/*
+Current limitation:
+  - Import's needs to be at the top of the file.
+  - Only: Import ... from "..." \n
+  - No require
+*/
+func (bundler *Bundler) lexer(keep bool) {
+
+	if !keep {
+		bundler.jsContent = ""
+	}
+
+	for _, entry := range bundler.jsPaths {
+		var file, err = os.ReadFile(entry)
+		if err != nil {
+			log.Fatalf("Could not read %s: \n %s", entry, err)
+		}
+
+		var tempBuf bytes.Buffer
+
+		tempBuf.Write(file)
+
+		var fileContent = tempBuf.String()
+
+		for _, line := range strings.Split(fileContent, "\n") {
+			trimmed := strings.TrimSpace(line)
+			if len(trimmed) > 0 {
+				if strings.HasPrefix(trimmed, "import") {
+					continue
+				}
+
+				if strings.HasPrefix(trimmed, "export") {
+					trimmed = trimmed[7:]
+				}
+
+				bundler.jsContent += trimmed
+				bundler.jsContent += "\n"
+			}
+		}
+	}
+
+}
+
+func (bundler *Bundler) printContent() {
+	fmt.Printf("%s \n", bundler.jsContent)
+}
+
+func (bundler *Bundler) printPaths() {
 	fmt.Printf("Index HTML File: \n")
 	fmt.Printf("    %s \n", bundler.indexPath)
 	fmt.Printf("JS Files: \n")
@@ -64,6 +113,9 @@ func main() {
 
 	bundler.iterateFolder(inputSrc, indexName)
 
-	bundler.print()
+	bundler.lexer(false)
+
+	bundler.printContent()
+	// bundler.printPaths()
 
 }
